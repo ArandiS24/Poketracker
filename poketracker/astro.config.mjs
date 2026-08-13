47cffa9736b47ee653f3b085c9f1fe457db1f69c
ArandiS24 <arandisanchezr@gmail.com>
2026-08-12 18:43:07 -0600

    Update astro.config.mjs

--- poketracker/astro.config.mjs
+++ poketracker/astro.config.mjs
@@ -4,17 +4,17 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-  preview: {
-    allowedHosts: true
-  }
-}
-    server: {
-      proxy: {
-        '/monitor-api': {
-          target: 'http://127.0.0.1:3784',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/monitor-api/, ''),
-        },
+    preview: {
+      allowedHosts: true
+    }
+  },
+  server: {
+    allowedHosts: true,
+    proxy: {
+      '/monitor-api': {
+        target: 'http://127.0.0.1:3784',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/monitor-api/, ''),
       },
     },
   },
